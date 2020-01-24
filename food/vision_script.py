6e016131c37fce9a34e87223932aaf786709b61f
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-01-24 13:09:53 +0000

    Added food functionality for nodeJS server :art:

--- food/vision_script.py
+++ food/vision_script.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
-
-import socket
 from google.cloud import vision
 import requests
 import os
 import io
-import pandas as pd
-import cv2
 
-max_layer = 4
+MAX_LAYER = 4
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'carbon-7fbf76411514.json'
 data = pd.read_csv("dataFake.csv", sep=';')
 
+
 def searchData(product):
     data = pd.read_csv("dataFake.csv", sep=';')
-    for i in range(0,len(data.index)-1):
-        if(data.loc[i]['typeFood']==product):
+    for i in range(0, len(data.index)-1):
+        if(data.loc[i]['typeFood'] == product):
             return data.loc[i]['Cfootprint']
     return None
 
-#Uses Google Vision API to get the labels of the picture.
-#@param content: holds content of the picture
+# Uses Google Vision API to get the labels of the picture.
+# @param content: holds content of the picture
+
+
 def get_vision_response(content):
     response = []
     client = vision.ImageAnnotatorClient()
-    image = vision.types.Image(content = content)
-    result = client.label_detection(image = image)
+    image = vision.types.Image(content=content)
+    result = client.label_detection(image=image)
     for i in range(0, len(result.label_annotations)):
         response.append(result.label_annotations[i].description)
     return response
 
-#Function which return the carbon footprint/kg of the first product label present in the data set.
-#If none of the product labels are present in the data set returns 0.
-#@param response: stores the labels of a product.
+# Function which return the carbon footprint/kg of the first product label present in the data set.
+# If none of the product labels are present in the data set returns 0.
+# @param response: stores the labels of a product.
+
+
 def first_layer_search(response):
     carbon = None
     for i in range(0, len(response)):
@@ -41,14 +42,17 @@ def first_layer_search(response):
             return carbon
     return carbon
 
-#This is called in get_carbon in case first_layer_search failed. It finds concepts related to product layers and searches
-#data set for their carbon footprint/kg. If none of the related concepts are present in the data set returns 0.
+# This is called in get_carbon in case first_layer_search failed. It finds concepts related to product layers and searches
+# data set for their carbon footprint/kg. If none of the related concepts are present in the data set returns 0.
+
+
 def next_layer_search(response):
     carbon = None
     next_response = []
     for i in range(0, len(response)):
         label = response[i]
-        obj = requests.get('http://api.conceptnet.io/query?start=/c/en/' + label + '&rel=/r/IsA&limit=1000').json() #DO WE NEED LIMIT????
+        obj = requests.get('http://api.conceptnet.io/query?start=/c/en/' +
+                           label + '&rel=/r/IsA&limit=1000').json()  # DO WE NEED LIMIT????
         for j in range(0, len(obj['edges'])):
             concept = obj['edges'][i]['end']['label']
             carbon = searchData(concept)
@@ -58,15 +62,17 @@ def next_layer_search(response):
     response = next_response
     return carbon
 
-#Function which returns the carbon footprint/kg of the product displayed in the picture. If carbon footprint/kg
-#is not found, returns 0.
-#@param content: holds content of the picture
+# Function which returns the carbon footprint/kg of the product displayed in the picture. If carbon footprint/kg
+# is not found, returns 0.
+# @param content: holds content of the picture
+
+
 def get_carbon(content):
     layer = 0
     carbon = None
     response = get_vision_response(content)
     carbon = first_layer_search(response)
-    while carbon == None and layer < max_layer:
+    while carbon == None and layer < MAX_LAYER:
         carbon = next_layer_search(response)
         layer += 1
     return carbon
--- node-serverless-graphql/index.js
+++ node-serverless-graphql/index.js
@@ -2,21 +2,60 @@ const express = require('express');
 const multer = require('multer')
 const app = express();
 const upload = multer()
+const mockFootprints = require('./mockFootprints.json')
+const vision = require('@google-cloud/vision');
+
+const MAX_TRY = 4
 
 app.get('/', (req, res) => {
-  console.log('Hello world received a request.');
-
-  const target = process.env.TARGET || 'Animal';
-  res.send(`Hello ${target}!`);
+  console.log({ mockFootprints })
+  res.send("API for CarbonFootprint is now accessible.");
 });
 
-app.post('/picture', upload.any(), (req, res) => {
-  console.log(req)
-  // run python script on req.files[0].buffer
-  res.send("24")
+app.post('/picture', upload.any(), async (req, res) => {
+  // TODO check to see if buffer exists. if not res.sendStatus(400) immediately
+  if (req.files === undefined) {
+    res.sendStatus(400)
+  }
+  const file = req.files[0].buffer
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  // Performs label detection on the image file
+  let labels;
+  try {
+    const [result] = await client.labelDetection(file);
+    labels = result.labelAnnotations;
+  } catch (err) {
+    console.error(err)
+  }
+
+  const carbonFootprint = getCarbonFootprint(labels)
+  console.log({ carbonFootprint })
+  if (!carbonFootprint) {
+    return res.sendStatus(400)
+  }
+
+  return res.send(carbonFootprint.toString())
+
 })
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('Hello world listening on port', port);
 });
+
+console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+
+const getCarbonFootprint = (labels) => {
+  console.log(labels)
+  for (let i = 0; i < MAX_TRY; i++) {
+    const name = labels[i].description.toLowerCase()
+    console.log(labels[i].description)
+    if (mockFootprints[name] !== undefined) {
+      return mockFootprints[name]
+    }
+  }
+  return null
+}
