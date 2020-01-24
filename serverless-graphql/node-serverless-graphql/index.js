3e7b07dc61d8ec57710e2de4a319b2e97166f258
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-01-24 10:08:20 +0000

    Added server, requests are working on RN

--- serverless-graphql/node-serverless-graphql/index.js
+++ serverless-graphql/node-serverless-graphql/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const multer = require('multer')
 const app = express();
+const upload = multer()
 
 app.get('/', (req, res) => {
   console.log('Hello world received a request.');
@@ -7,6 +9,12 @@ app.get('/', (req, res) => {
   const target = process.env.TARGET || 'Animal';
   res.send(`Hello ${target}!`);
 });
+
+app.post('/picture', upload.any(), (req, res) => {
+  console.log(req)
+  // run python script on req.files[0].buffer
+  res.send("24")
+})
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
