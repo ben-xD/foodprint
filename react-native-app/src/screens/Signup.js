74b2cddb335b7233aa40c65272de3c2bde74a7c3
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-02-02 15:22:26 +0000

    Fixed formatting for signup

--- react-native-app/src/screens/Signup.js
+++ react-native-app/src/screens/Signup.js
@@ -1,21 +1,17 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
 import { Button, Input, Text } from 'react-native-elements';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [passwordConfirm, setPasswordConfirm] = useState('');
 
   const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   const signUpHandler = () => {
     if (!EMAIL_REGEX.test(email)) {
       return console.warn('alert user, email invalid.');
-    }
-
-    if (password !== passwordConfirm) {
-      return console.warn('alert user, mismatched password');
     }
 
     if (password.length < 8) {
@@ -31,10 +27,9 @@ export default Signup = () => {
     console.log({ userDetails });
   };
 
-
   return (
-    <View style={{ width: '100%', flex: 1, alignItems: 'center' }}>
-      <View style={{ alignItems: 'center', justifyContent: 'center', marginVertical: 100 }}>
+    <ScrollView contentContainerStyle={{ alignItems: 'center' }} style={{ width: '100%', height: '100%' }}>
+      <View style={{ alignItems: 'center', justifyContent: 'center', marginVertical: 50 }}>
         <Text h1>
           FoodPrint
           </Text>
@@ -43,17 +38,16 @@ export default Signup = () => {
         <View>
           <Input label={'Your Email Address'} placeholder="banana@foodprint.co" value={email} onChangeText={value => setEmail(value)} />
           <Input label={'Password'} placeholder="Password" value={password} onChangeText={value => setPassword(value)} secureTextEntry={true} />
-          <Input label={'Confirm Password'} placeholder="Confirm password" value={passwordConfirm} onChangeText={value => setPasswordConfirm(value)} secureTextEntry={true} />
         </View>
       </View>
-      <View style={{ width: '80%', position: 'absolute', bottom: 50 }}>
+      <View style={{ width: '80%' }}>
         <Button
-          buttonStyle={{ backgroundColor: 'green' }}
+          buttonStyle={{ backgroundColor: 'green', marginVertical: 100 }}
           titleStyle={{ fontSize: 24 }}
           title="Register"
           onPress={signUpHandler}
         />
       </View>
-    </View>
+    </ScrollView>
   );
 };
