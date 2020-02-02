23b09b13db4f96abb1c00d49edd435306d68bed3
Sander Coates <sandercoates@gmail.com>
2020-02-02 14:35:43 +0000

    wip

--- node-serverless-graphql/schema.js
+++ node-serverless-graphql/schema.js
@@ -2,17 +2,19 @@ const {gql} = require('apollo-server');
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql` 
-  type Picture {
-    title: String
-    image: String
+  type File {
+    id: ID!
+    path: String!
+    filename: String!
+    mimetype: String!
   }
   
   type Query {
-    getPicture(title: String!): Picture
+    uploads: String
   }
   
   type Mutation {
-    postPicture(title: String): Picture
+    singleUpload(file: Upload!): File!
   }
 `;
 
