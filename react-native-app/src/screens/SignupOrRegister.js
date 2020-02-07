e3aeb6d09cd8a59437e5692d29340539747e1837
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-02-07 10:23:07 +0000

    Reformatted react native register screen refactor. Adding google sign in option for future.

--- react-native-app/src/screens/SignupOrRegister.js
+++ react-native-app/src/screens/SignupOrRegister.js
@@ -5,18 +5,28 @@ import Config from 'react-native-config';
 
 
 export default SignupOrRegister = ({ navigation }) => {
+  const signInWithGoogle = () => {
+    console.log('Google sign in not implemented');
+  };
+
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text h1 h1Style={{}}>
+    <View style={{ width: '100%', height: '100%', alignItems: 'center' }}>
+      <View style={{ alignItems: 'center', justifyContent: 'center', marginVertical: 50 }}>
+        <Text h1>
           FoodPrint
         </Text>
       </View>
-      <View style={{ flex: 1 }} />
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <View style={{ flex: 1 }} />
-        <View style={{ flex: 4, flexDirection: 'column' }}>
+      <View style={{ width: '80%', position: 'absolute', bottom: 48 }}>
+        <View>
           <Button
+            containerStyle={{ marginVertical: 8 }}
+            buttonStyle={{ backgroundColor: 'grey' }}
+            titleStyle={{ fontSize: 24 }}
+            title="Login with Google"
+            onPress={() => signInWithGoogle()}
+          />
+          <Button
+            containerStyle={{ marginVertical: 8 }}
             buttonStyle={{ backgroundColor: 'green' }}
             titleStyle={{ fontSize: 24 }}
             title="Register"
@@ -29,9 +39,8 @@ export default SignupOrRegister = ({ navigation }) => {
             type="clear"
           />
         </View>
-        <View style={{ flex: 1 }} />
       </View>
-    </View>
+    </View >
   );
 }
   ;
