b421989735afdfcaa7b6750e54273990b6f2f277
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-02-07 10:43:36 +0000

    Removed gql from index, as its being used in App (creating client twice previously)

--- react-native-app/index.js
+++ react-native-app/index.js
@@ -1,19 +1,5 @@
 import { AppRegistry } from 'react-native';
-import Root from './App';
+import App from './App';
 import { name as appName } from './app.json';
-import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-
-// Create the client as outlined in the setup guide
-const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-});
-
-const App = () => (
-  <ApolloProvider client={client}>
-    <Root />
-  </ApolloProvider>
-);
 
 AppRegistry.registerComponent(appName, () => App);
