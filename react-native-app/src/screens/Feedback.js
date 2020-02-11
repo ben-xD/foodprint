66c686b884ed9898a8ac84e6abc15eedee164858
Sandrine Chausson <slc19@ic.ac.uk>
2020-02-11 11:45:12 +0000

    Fixed UI for Feedback and Camera

--- react-native-app/src/screens/Feedback.js
+++ react-native-app/src/screens/Feedback.js
@@ -23,16 +23,26 @@ const Feedback = ({ route, navigation }) => {
   const [postPictureMutation, { loading, error, data }] = useMutation(POST_PICTURE_MUTATION);
 
   function calculateRating(carbonFootprint) {
-    if (carbonFootprint < 4) {
+    if (carbonFootprint < 2) {
       return 5;
-    } else if (carbonFootprint < 8) {
+    } else if (carbonFootprint < 4) {
+      return 4.5;
+    } else if (carbonFootprint < 6) {
       return 4;
-    } else if (carbonFootprint < 12) {
+    } else if (carbonFootprint < 8) {
+      return 3.5;
+    } else if (carbonFootprint < 10) {
       return 3;
-    } else if (carbonFootprint < 16) {
+    } else if (carbonFootprint < 12) {
+      return 2,5;
+    } else if (carbonFootprint < 14) {
       return 2;
-    } else if (carbonFootprint < 20) {
+    } else if (carbonFootprint < 16) {
+      return 1.5;
+    } else if (carbonFootprint < 18) {
       return 1;
+    } else if (carbonFootprint < 20) {
+      return 0.5;
     } else {
       return 0;
     }
@@ -44,11 +54,9 @@ const Feedback = ({ route, navigation }) => {
       setMeal({
         ...meal,
         score: 9000,
-        // score: data.postPicture.product
         description: data.postPicture.product.name,
       });
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
   useEffect(() => {
@@ -66,7 +74,6 @@ const Feedback = ({ route, navigation }) => {
       });
       classifyPicture();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [image]);
 
   return (
@@ -75,9 +82,9 @@ const Feedback = ({ route, navigation }) => {
         isVisible={isVisible}
         onBackdropPress={() => setVisibility(false)}
       >
-        <View style={{ flex: 1 }}>
-          <View style={{ flexDirection: 'row', flex: 2, alignItems: 'center', justifyContent: 'center' }}>
-            <Text h3 style={{ flex: 5, textAlign: 'center' }}>We're sorry we couldn't find your item...</Text>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <View style={{flex:2, justifyContent:'center'}}>
+            <Text h3 style={{ textAlign: 'center' }}>We're sorry we couldn't find your item...</Text>
           </View>
           <View style={{ flex: 2, alignItems: 'center', justifyContent: 'center' }}>
             <Image
@@ -85,48 +92,39 @@ const Feedback = ({ route, navigation }) => {
               source={{ uri: 'https://mir-s3-cdn-cf.behance.net/project_modules/disp/fc4a1059120725.5a15c9fa08f78.gif' }}
             />
           </View>
-          <View style={{ flexDirection: 'row', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={{ fontSize: 18, flex: 4, textAlign: 'center' }}>Let us know what it was, so we can improve our
-                app:</Text>
+          <View style={{ flex:1, justifyContent:'center'}}>
+            <Text style={{ fontSize: 22, textAlign: 'center', margin:20 }}>Let us know what it was, so we can improve our
+              app:</Text>
           </View>
-          <View style={{ flex: 1.5, flexDirection: 'row', justifyContent:'center' }}>
-            <View style={{ flex: 0.8, flexDirection: 'column', justifyContent: 'center' }}>
-              <Input
-                placeholder="e.g. Cucumber"
-              />
+          <View style={{flex:1.5 }}>
+            <View style={{margin:20}}>
+              <Input placeholder="e.g. Cucumber"/>
               <Button
                 buttonStyle={{ backgroundColor: 'green', marginTop:20 }}
                 titleStyle={{ fontSize: 24 }}
                 title="Submit"
                 onPress={() => alert('Implement')}
               />
-              {/*<View style={{ height: 50 }} />*/}
             </View>
           </View>
         </View>
       </Overlay>
 
 
-      <View style={{ flex: 1 }} />
-      <View style={{ flex: 3.5, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
-        <Image
-          style={{ height: 200, width: 200 }}
-          source={{ uri: meal.uri }}
-        />
-        <View style={{ height: 10 }} />
-        <Text h2>{meal.description}</Text>
-        <View style={{ height: 10 }} />
-        <Rating
-          readonly
-          startingValue={calculateRating(meal.score)}
-        />
-        <View style={{ height: 10 }} />
-        <Text style={{ fontSize: 18 }}>{meal.score}kg of CO2 eq/kg</Text>
-      </View>
-      <View style={{ flex: 0.5 }} />
-      <View style={{ flex: 2, flexDirection: 'row' }}>
-        <View style={{ flex: 1 }} />
-        <View style={{ flex: 4, flexDirection: 'column' }}>
+      <View style={{flex:1, justifyContent:'center'}}>
+        <View style={{ flex: 4, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white', margin:10, marginTop:100, marginBottom:30 }}>
+          <Image
+            style={{ height: 350, width: 350 }}
+            source={{ uri: meal.uri }}
+          />
+          <Text h2 style={{marginTop:20, marginBottom:10}}>{meal.description}</Text>
+          <Rating
+            readonly
+            startingValue={calculateRating(meal.score)}
+          />
+          <Text style={{ fontSize: 18, margin:10 }}>{meal.score}kg of CO2 eq/kg</Text>
+        </View>
+        <View style={{ flex: 1, flexDirection: 'column', marginLeft:50, marginRight:50 }}>
           <Button
             buttonStyle={{ backgroundColor: 'darkred' }}
             titleStyle={{ fontSize: 24 }}
@@ -134,7 +132,6 @@ const Feedback = ({ route, navigation }) => {
             onPress={() => setVisibility(true)}
           />
         </View>
-        <View style={{ flex: 1 }} />
       </View>
     </View>
   );
