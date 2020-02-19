21744e7f83c2f3afdd952a7a4792923fd4e75f90
Sander Coates <sandercoates@gmail.com>
2020-02-19 18:04:42 +0000

    get carbon footprint from image test implemented

--- node-serverless-graphql/carbon-footprint/tests/carbon_footprint_calculation.test.js
+++ node-serverless-graphql/carbon-footprint/tests/carbon_footprint_calculation.test.js
@@ -1,6 +1,16 @@
 const carbon_footprint_calculation = require('../carbon_footprint_calculation');
+const fs = require('fs');
 
 test('getCarbonFootprintFromName: Simple test with an item in the database (rice)', async () => {
   let response = await carbon_footprint_calculation.getCarbonFootprintFromName("rice");
   expect(response).toEqual({item: "rice", carbonFootprintPerKg: 1.14});
+});
+
+test('getCarbonFootprintFromImage: orange image', async() => {
+  const  path = __dirname + "/orange_image.txt";
+  const image  = fs.readFileSync(path); // Read text file
+  const image_string = image.toString('utf-8') // Convert to string
+  const image_buffer = new Buffer(image_string, 'base64'); // Decode as base64 file buffer
+  let response = await carbon_footprint_calculation.getCarbonFootprintFromImage(image_buffer);
+  expect(response).toEqual({item: "citrus", carbonFootprintPerKg: 0.5});
 });
