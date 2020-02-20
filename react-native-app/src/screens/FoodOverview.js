a6451544f8079f0e4a755ad07e85d3a2d211e17a
Sandrine Chausson <slc19@ic.ac.uk>
2020-02-20 19:00:34 +0000

    Fixed UI for IPhone 7 Plus (device usually used for demos)

--- react-native-app/src/screens/FoodOverview.js
+++ react-native-app/src/screens/FoodOverview.js
@@ -8,9 +8,11 @@ import {
   ScrollView,
 } from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import { Overlay } from 'react-native-elements';
 
 const FoodOverview = ({navigation}) => {
   const [food, setFood] = useState([]);
+  const [isVisible, setVisibility] = useState(true);
 
   // useEffect(() => {
   //   navigation.navigate('Camera');
@@ -41,22 +43,24 @@ const FoodOverview = ({navigation}) => {
           <MaterialCommunityIcons name="settings-outline" color={'grey'} size={50} style={{ margin:10}} />
         </TouchableOpacity>
       </View>
-      <ScrollView>
-        <View style={{justifyContent:'center', alignItems:'center', marginHorizontal:40}}>
-          <Text style={{ fontSize: 36, marginVertical: 20, textAlign:"center"}}>Welcome to FoodPrint!</Text>
-          <Text style={{fontSize:20, textAlign:'center', marginTop:20}}>
-            You can now know the carbon footprint of the food you buy simply by scanning its barcode or taking a picture
-            of it!
-          </Text>
-          <Image
-            source={{ uri:'https://png2.cleanpng.com/sh/4618a1d69c3326bc75ab0d6a0d3cd256/L0KzQYm3VcIyN5NvjJH0aYP2gLBuTfVud6Vue9H3LXXwf7vwTgN1cZRwfeQ2ZnHmdbP2jBsucZ9oReV2aXzoiX68gsAzPWNnfKdrM0LmQXA9WMIyOGE7T6MAMka7R4mCUMk0P2Q7RuJ3Zx==/kisspng-emoticon-emoji-sticker-facebook-inc-smiley-5b0252bd5b32c1.6821006715268789093736.png'}}
-            style={{ width: 200, height:200, marginTop:10}}
-          />
-          <Text style={{fontSize:20, textAlign:'center', marginTop:20}}>
-            To get started, simply click on the Camera icon in the top left corner!
-          </Text>
-        </View>
-      </ScrollView>
+      <Overlay isVisible = { isVisible } onBackdropPress = {() => setVisibility(false)}>
+        <ScrollView>
+          <View style={{justifyContent:'center', alignItems:'center', marginHorizontal:40}}>
+            <Text style={{ fontSize: 36, marginVertical: 20, textAlign:"center"}}>Welcome to FoodPrint!</Text>
+            <Text style={{fontSize:20, textAlign:'center', marginTop:20}}>
+              You can now know the carbon footprint of the food you buy simply by scanning its barcode or taking a picture
+              of it!
+            </Text>
+            <Image
+              source={{ uri:'https://png2.cleanpng.com/sh/4618a1d69c3326bc75ab0d6a0d3cd256/L0KzQYm3VcIyN5NvjJH0aYP2gLBuTfVud6Vue9H3LXXwf7vwTgN1cZRwfeQ2ZnHmdbP2jBsucZ9oReV2aXzoiX68gsAzPWNnfKdrM0LmQXA9WMIyOGE7T6MAMka7R4mCUMk0P2Q7RuJ3Zx==/kisspng-emoticon-emoji-sticker-facebook-inc-smiley-5b0252bd5b32c1.6821006715268789093736.png'}}
+              style={{ width: 150, height:150, marginTop:10}}
+            />
+            <Text style={{fontSize:20, textAlign:'center', marginTop:20}}>
+              To get started, simply click on the Camera icon in the top left corner!
+            </Text>
+          </View>
+        </ScrollView>
+      </Overlay>
     </SafeAreaView>
   );
 };
