54af7abd6d5faff09a6a19947f2779a79499260a
Dan Hausner <dn.hausner@gmail.com>
2020-02-25 19:28:44 +0000

    Create snapshot testing for the ErrorMessage component

---
+++ react-native-app/src/components/__tests__/ErrorMessage-test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ErrorMessage from '../ErrorMessage';
+import {useMutation} from '@apollo/react-hooks';
+
+import renderer from 'react-test-renderer';
+
+jest.mock('@apollo/react-hooks');
+
+// Check current component against its snapshot
+test('ErrorMessage renders correctly and is the same as its previous snapshot', () => {
+  useMutation.mockImplementation(() => [null, {loading: null, error: null, data: null}]);
+  const tree = renderer.create(<ErrorMessage isVisible={true}/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
