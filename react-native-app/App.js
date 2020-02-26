0baeb98e8298aae64a13b8bbd5ea749b2086f403
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-02-26 08:59:42 +0000

    Adding todo comment

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -149,6 +149,7 @@ const App = () => {
           console.log({ userCredentials });
         } catch (e) {
           if (e.code === 'auth/email-already-in-use') {
+            // TODO can this function return an Promise<error> back to Signup.js. Then display it there
             return console.warn('tell user email is already used');
           } else if (e.code === 'auth/invalid-email') {
             return console.warn('tell user invalid email');
