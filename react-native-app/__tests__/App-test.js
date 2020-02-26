8fa3a7669e96962e1b127f6a2865be49baa0230c
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-02-26 07:41:29 +0000

    Updating style formatting for NoInternet

--- react-native-app/__tests__/App-test.js
+++ react-native-app/__tests__/App-test.js
@@ -3,7 +3,6 @@ import { } from 'react-native';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import NoInternet from '../src/screens/NoInternet';
-import Signup from '../src/screens/Signup';
 
 jest.mock('react-native-camera', () => 'Camera');
 jest.mock('@react-native-community/async-storage', () => { });
