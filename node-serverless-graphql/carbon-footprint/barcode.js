f29da370f68aec8d5068ebf50de7148689e98791
Dan Hausner <dn.hausner@gmail.com>
2020-03-16 22:00:35 +0000

    Delete unused import of mongooseQueries in barcode

--- node-serverless-graphql/carbon-footprint/barcode.js
+++ node-serverless-graphql/carbon-footprint/barcode.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const { getCarbonFootprintFromName} = require('./carbon_footprint_calculation');
-const mongooseQueries = require('../datasources/carbon');
 
 //contains the key to query the Tesco API
 const options = {
