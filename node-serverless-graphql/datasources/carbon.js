73232bab55a3882dbe8ba5877c899280db7c89aa
Dan Hausner <dn.hausner@gmail.com>
2020-03-21 17:25:51 +0000

    Create CarbonAPI class from the functions in carbon.js. Keep the functional implementation wherever this is used

--- node-serverless-graphql/datasources/carbon.js
+++ node-serverless-graphql/datasources/carbon.js
@@ -3,6 +3,37 @@ const config = require('../carbon-footprint/config');
 
 let carbonSchema;
 
+class CarbonAPI {
+
+  async connect() {
+    try{
+      await mongoose.connect(config.dbServer, { useNewUrlParser: true, useUnifiedTopology: true });
+    } catch(error){
+      handleError(error);
+    }
+  }
+
+  async getCarbonFootprintModel() {
+    if (!carbonSchema) {
+      carbonSchema = new mongoose.Schema({
+        item: String,
+        carbonpkilo: Number,
+      }, { collection: 'carbon' });
+    }
+
+    return mongoose.model('Carbon', carbonSchema);
+  }
+
+  async disconnect() {
+    mongoose.disconnect((err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+
+}
+
 const connect = async ()  => {
   try{
     await mongoose.connect(config.dbServer, { useNewUrlParser: true, useUnifiedTopology: true });
