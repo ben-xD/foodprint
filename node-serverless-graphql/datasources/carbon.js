14475b638f6eaccc7fa20cd2c632dcc8d6aea369
Dan Hausner <dn.hausner@gmail.com>
2020-03-21 21:28:18 +0000

    Remove functional implementation of the database, which is no longer used

--- node-serverless-graphql/datasources/carbon.js
+++ node-serverless-graphql/datasources/carbon.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const config = require('../carbon-footprint/config');
 
-let carbonSchema;
-
 class CarbonAPI {
 
   constructor() {
-    connect();
+    this.carbonSchema;
+    this.searchData = this.searchData.bind(this);
+    this.connect();
   }
 
   async connect() {
@@ -18,17 +18,6 @@ class CarbonAPI {
     }
   }
 
-  async getCarbonFootprintModel() {
-    if (!carbonSchema) {
-      carbonSchema = new mongoose.Schema({
-        item: String,
-        carbonpkilo: Number,
-      }, {collection: 'carbon'});
-    }
-
-    return mongoose.model('Carbon', carbonSchema);
-  }
-
   async disconnect() {
     mongoose.disconnect((err) => {
       if (err) {
@@ -37,8 +26,19 @@ class CarbonAPI {
     });
   }
 
+  getCarbonFootprintModel() {
+    if (!this.carbonSchema) {
+      this.carbonSchema = new mongoose.Schema({
+        item: String,
+        carbonpkilo: Number,
+      }, {collection: 'carbon'});
+    }
+
+    return mongoose.model('Carbon', this.carbonSchema);
+  }
+
   async searchData(label) {
-    const carbonModel = getCarbonFootprintModel();
+    const carbonModel = this.getCarbonFootprintModel();
     let itemList;
     try {
       await carbonModel.findOne({item: label}, (err, items) => {
@@ -56,53 +56,6 @@ class CarbonAPI {
   }
 
 }
-
-const connect = async () => {
-  try {
-    await mongoose.connect(config.dbServer, {useNewUrlParser: true, useUnifiedTopology: true});
-  } catch (error) {
-    handleError(error);
-  }
-};
-
-const getCarbonFootprintModel = () => {
-  if (!carbonSchema) {
-    carbonSchema = new mongoose.Schema({
-      item: String,
-      carbonpkilo: Number,
-    }, {collection: 'carbon'});
-  }
-
-  return mongoose.model('Carbon', carbonSchema);
-};
-
-const disconnect = () => {
-  mongoose.disconnect((err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-};
-
-// Function that tries to find a label in the DB.
-// @return cabonpkilo (if found) or undefined (if not found)
-const searchData = async (label) => {
-  const carbonModel = getCarbonFootprintModel();
-  let itemList;
-  try {
-    await carbonModel.findOne({item: label}, (err, items) => {
-      if (err) {
-        throw err;
-      }
-      itemList = items;
-    }).exec();
-
-    return itemList.carbonpkilo;
-
-  } catch (err) {
-    return undefined;
-  }
-};
 
 const CarbonAPIInstance = new CarbonAPI();
 
