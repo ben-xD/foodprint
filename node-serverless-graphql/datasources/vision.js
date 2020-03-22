ddc85f11314804957f8387978e7068eaf863d754
Dan Hausner <dn.hausner@gmail.com>
2020-03-22 13:28:08 +0000

    Wrap Google Vision in VisionAPI class

--- node-serverless-graphql/datasources/vision.js
+++ node-serverless-graphql/datasources/vision.js
@@ -1,45 +1,57 @@
 const vision = require('@google-cloud/vision');
 const credentials = require('../credentials/carbon-7fbf76411514.json');
 
-/**
- * Function to return descriptor labels for input image, using Google Vision API
- * Example labels at https://docs.google.com/spreadsheets/d/1MQl5HjTbkToTniYZ3w6wwPfPen9yEGbur-11XgVCIT8/edit?usp=sharing
- * @param {Buffer} image - image file buffer
- * @returns {Array} List of string labels (e.g. "coffee")
-*/
-const getImageLabels = async (image) => {
-  // Setup Google Vision ImageAnnotatorClient
-  let client;
-  try {
-    // Using Alba's credentials here
-    client = new vision.ImageAnnotatorClient({ credentials });
-  } catch (err) {
-    console.log('Failed to set up Google Vision ImageAnnotatorClient.');
-    console.log(err);
+class VisionAPI {
+
+  constructor(credentials) {
+
+    this.getImageLabels = this.getImageLabels.bind(this);
+
+    // Setup Google Vision ImageAnnotatorClient
+    try {
+      // Using Alba's credentials here
+      this.client = new vision.ImageAnnotatorClient({credentials});
+      console.log('VisionAPI: connected to Google Vision.')
+    } catch (err) {
+      console.error('Failed to set up Google Vision ImageAnnotatorClient.');
+      console.error(err);
+    }
   }
 
-  // Request label detection
-  let googleResult;
-  try {
-    googleResult = await client.labelDetection(image);
-  } catch (err) {
-    console.log('Google Vision labelDetection failed.');
-    console.log(err);
+  /**
+   * Method to return descriptor labels for input image, using Google Vision API
+   * Example labels at https://docs.google.com/spreadsheets/d/1MQl5HjTbkToTniYZ3w6wwPfPen9yEGbur-11XgVCIT8/edit?usp=sharing
+   * @param {Buffer} image - image file buffer
+   * @returns {Array} List of string labels (e.g. "coffee")
+   */
+  async getImageLabels(image) {
+
+    // Request label detection
+    let googleResult;
+    try {
+      googleResult = await this.client.labelDetection(image);
+    } catch (err) {
+      console.log('Google Vision labelDetection failed.');
+      console.log(err);
+    }
+
+    // Get labelAnnotations from Google result
+    const {labelAnnotations} = googleResult[0];
+
+    // Store lower case label descriptions in array
+    let labels = [];
+    for (let i = 0; i < labelAnnotations.length; i++) {
+      labels.push(labelAnnotations[i].description.toLowerCase())
+    }
+
+    // Remove duplicates and return
+    labels = labels.filter((element, first_index) => labels.indexOf(element) === first_index);
+
+    return labels;
   }
 
-  // Get labelAnnotations from Google result
-  const { labelAnnotations } = googleResult[0];
+}
 
-  // Store lower case label descriptions in array
-  let labels = [];
-  for (let i = 0; i < labelAnnotations.length; i++) {
-    labels.push(labelAnnotations[i].description.toLowerCase())
-  }
+const VisionAPIInstance = new VisionAPI(credentials);
 
-  // Remove duplicates and return
-  labels = labels.filter((element, first_index) => labels.indexOf(element) === first_index)
-
-  return labels;
-};
-
-module.exports = getImageLabels;
+module.exports = VisionAPIInstance.getImageLabels;
