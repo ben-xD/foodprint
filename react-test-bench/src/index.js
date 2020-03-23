3a4fb9fe266e6a076fdb5f7b1617ecc4b81ed235
Dan Hausner <dn.hausner@gmail.com>
2020-03-23 22:25:43 +0000

    Query an example currency API from index.js

--- react-test-bench/src/index.js
+++ react-test-bench/src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
+
+client
+.query({
+  query: gql`
+      {
+          rates(currency: "USD") {
+              currency
+          }
+      }
+  `
+})
+.then(result => console.log(result));
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
