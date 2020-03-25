65af027907124939421938b09040ece44363db4e
Dan Hausner <dn.hausner@gmail.com>
2020-03-25 08:03:45 +0000

    Increase timeout for tests to accommodate slow database queries

--- node-serverless-graphql/tests/resolvers.test.js
+++ node-serverless-graphql/tests/resolvers.test.js
@@ -15,6 +15,7 @@ const dataSources = {
 };
 
 test('getCarbonFootprintFromName: Simple test with an item in the database (rice)', async () => {
+  jest.setTimeout(30000);
   const name = 'rice';
   const actual = await resolvers.Mutation.postCorrection(null, {name}, {dataSources});
   const expected = {product: {name: "rice"}, carbonFootprintPerKg: 1.14};
@@ -22,12 +23,11 @@ test('getCarbonFootprintFromName: Simple test with an item in the database (rice
 });
 
 test('getCarbonFootprintFromImage: rice image', async () => {
-  jest.setTimeout(10000);
+  jest.setTimeout(30000);
   const file = {base64: rice_image};
   let actual = await resolvers.Mutation.postPicture(null, {file}, {dataSources}, null);
   const expected = {product: {name: "rice"}, carbonFootprintPerKg: 1.14};
   expect(actual).toEqual(expected);
-  jest.setTimeout(5000);
 });
 
 test('Query: not used for anything yet, mandatory for GraphQL', async () => {
