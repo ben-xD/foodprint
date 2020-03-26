731973d66d0ccac47edc804b8d1ae8134692beda
Dan Hausner <dn.hausner@gmail.com>
2020-03-26 00:24:37 +0000

    Fix resovlers.js, comment out resolver that is not implemented in the schema yet

--- node-serverless-graphql/resolvers.js
+++ node-serverless-graphql/resolvers.js
@@ -55,11 +55,11 @@ const resolvers = {
       console.log({ 'Returning': response });
       return response;
     },
-    postUserHistoryEntry: async (parent, { entry }, context) => {
-      console.log({ entry });
-      console.log('Received postUserHistoryEntry mutation...');
-      return true;
-    },
+    // postUserHistoryEntry: async (parent, { entry }, context) => {
+    //   console.log({ entry });
+    //   console.log('Received postUserHistoryEntry mutation...');
+    //   return true;
+    // },
   },
 };
 
