4e17890b8dfc0e8ca1b4bb8adb26b931caf87787
Dan Hausner <dn.hausner@gmail.com>
2020-03-26 00:50:31 +0000

    Implement the new GraphQL schema for the current functionality

--- node-serverless-graphql/schema.js
+++ node-serverless-graphql/schema.js
@@ -1,37 +1,32 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-  scalar Upload
+    scalar Upload
 
 
-  type Product {
-    name: String
-  }
+    type ProductFootprint {
+        name: String
+        carbonFootprintPerKg: Float
+    }
 
-  input PictureFile {
-    base64: String
-    uri: String
-    height: Int
-    width: Int
-    pictureOrientation: Int
-    deviceOrientation: Int
-  }
- 
-  type CarbonFootprintReport {
-    product: Product
-    carbonFootprint: Float
-    carbonFootprintPerKg: Float
-  }
-  
-  type Query {
-    _: String
-  }
-  
-  type Mutation {
-    postPicture(file: PictureFile): CarbonFootprintReport
-    postBarcode(barcode: String!): CarbonFootprintReport
-    postCorrection(name: String!): CarbonFootprintReport
-  }
+    input PictureFile {
+        base64: String
+        uri: String
+        height: Int
+        width: Int
+        pictureOrientation: Int
+        deviceOrientation: Int
+    }
+
+    type Query {
+        _: String
+    }
+
+    type Mutation {
+        postPicture(file: PictureFile): ProductFootprint
+        postBarcode(barcode: String!): ProductFootprint
+        postCorrection(name: String!): ProductFootprint
+    }
 `;
 
 // Construct a schema, using GraphQL schema language
