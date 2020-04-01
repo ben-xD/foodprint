aaebd7af3dbaccdeb5527c12c3f26ced66cf91cf
Laura Caune <lauracaune4@gmail.com>
2020-04-01 12:27:58 +0100

    Replaced Tesco API with a simple lookup (need to add more products)

--- node-serverless-graphql/carbon-footprint/barcode_without_Tesco_API.js
+++ node-serverless-graphql/carbon-footprint/barcode_without_Tesco_API.js
@@ -17,6 +17,4 @@ const getCarbonFootprintFromBarcode = async (dataSources, barcode) => {
 
 };
 
-module.exports = {
-    getCarbonFootprintFromBarcode
-};
+module.exports = { getCarbonFootprintFromBarcode };
