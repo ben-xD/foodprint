12052a5340b2dc16bb474d8215f4f8cd18428a3a
Dan Hausner <dn.hausner@gmail.com>
2020-04-01 16:27:00 +0100

    Improve tests in carbon.test.js to reflect the recent changes in carbon.js

--- node-serverless-graphql/datasources/tests/carbon.test.js
+++ node-serverless-graphql/datasources/tests/carbon.test.js
@@ -1,19 +1,18 @@
 const CarbonAPI = require('../carbon');
 
-const  { createStore, deleteStore } = require('../../utils');
+const {createStore, deleteStore} = require('../../utils');
 const store = createStore();
 
 let carbonAPI = new CarbonAPI(store);
 
-describe('Real database', () => {
+describe('getCfOneItem: Real database', () => {
 
   test('Carbon footprint of rice is 1.14 and category is 1000', async () => {
     jest.setTimeout(10000);
     const name = 'rice';
-    const expected = { carbonpkilo: 1.14, categories: '1000' };
+    const expected = {carbonpkilo: 1.14, categories: '1000'};
     expect.assertions(1);
     const res = await carbonAPI.getCfOneItem(name);
-    console.log('res', res);
     const actual = res;
     expect(actual).toMatchObject(expected);
   });
@@ -24,10 +23,36 @@ describe('Real database', () => {
     const expected = null;
     expect.assertions(1);
     const res = await carbonAPI.getCfOneItem(name);
-    console.log('res', res);
     const actual = res;
     expect(actual).toEqual(expected);
   });
 
-})
+});
+
+describe('getCfMultipleItems: Real database', () => {
+
+  test('Carbon footprint of rice and orange is 1.14 and 0.5 respectively', async () => {
+    jest.setTimeout(10000);
+    const labelList = ['orange', 'rice'];
+    const expected = [
+      {item: 'orange', carbonpkilo: 0.5, categories: '1000'},
+      {item: 'rice', carbonpkilo: 1.14, categories: '1000'}
+    ];
+    expect.assertions(1);
+    const res = await carbonAPI.getCfMultipleItems(labelList);
+    expect(res).toMatchObject(expected);
+  });
+
+  test('Querying one known item and one unkown item returns a list with values only for the known item', async () => {
+    jest.setTimeout(10000);
+    const labelList = ['rice', 'some-veryRanDOOMiteeem:P'];
+    const expected = [
+      {item: 'rice', carbonpkilo: 1.14, categories: '1000'},
+    ];
+    expect.assertions(1);
+    const res = await carbonAPI.getCfMultipleItems(labelList);
+    expect(res).toMatchObject(expected);
+  });
+
+});
 
