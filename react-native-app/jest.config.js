269146faa729137cfad6422772473b385c6b9c77
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-04-03 11:47:59 +0100

    Jest config uses object.assign and destructuring to avoid overriding preset

--- react-native-app/jest.config.js
+++ react-native-app/jest.config.js
@@ -1,15 +1,14 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-module.exports = {
+// We use it as a preset, but also load it to allow 
+const jestPreset = require('@testing-library/react-native/jest-preset');
+
+module.exports = Object.assign(jestPreset, {
   setupFiles: [
-    './jest.setup.js',
+    ...jestPreset.setupFiles,
     './node_modules/react-native-gesture-handler/jestSetup.js',
   ],
-  preset: '@testing-library/react-native',
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['./src/teardown.js'],
@@ -17,9 +16,19 @@ module.exports = {
   // The test environment that will be used for testing
   testEnvironment: 'node',
 
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    'mockedResponses',
-    // "/node_modules/"
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // We cannot re use the old ignore pattern (using ...jestPreset.transformIgnorePatterns), 
+  // because then the 2 lines would 'ignore' each other. 
+  // Instead, we manually join them into one ignore pattern
+  transformIgnorePatterns: [
+    "node_modules/(?!victory|react-native-svg|@react-native-community/*|react-native.*|@?react-navigation.*)",
   ],
-};
+
+  // Added according to [victory](https://formidable.com/open-source/victory/docs/native/)
+  transform: {
+    ...jestPreset.transform,
+    "^.+\\.jsx?$": "babel-jest"
+  },
+})
