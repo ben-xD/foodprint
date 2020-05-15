55ff546b0630cbd18b5ba2ff15d09398afd61875
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-05-15 22:20:30 +0100

    Fix link on site

--- gatsby-site/src/components/header.js
+++ gatsby-site/src/components/header.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const Header = ({ siteTitle }) => (
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">{siteTitle}</a>
+    <a class="navbar-brand" href="/">{siteTitle}</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
