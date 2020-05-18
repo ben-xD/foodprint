4fac375aa61e8ac93910eb1e88d47c786c9730c9
Ben Butterworth <24711048+ButterHub@users.noreply.github.com>
2020-05-18 13:57:34 +0100

    Update header to have styling

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#117F13`,
         theme_color: `#117F13`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
